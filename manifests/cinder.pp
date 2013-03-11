1a088c40c91585e31da8d649a85c40675df781c5
zestrada <countach44@gmail.com>
2013-03-11 14:20:51 -0500

    Added a cinder::api instance to openstack::cinder

--- manifests/cinder.pp
+++ manifests/cinder.pp
@@ -2,9 +2,20 @@ class openstack::cinder(
   $sql_connection,
   $rabbit_password,
   $iscsi_ip_address,
-  $rabbit_host     = '127.0.0.1',
-  $volume_group    = 'nova-volumes',
-  $enabled         = true
+  $rabbit_host            = '127.0.0.1',
+  $volume_group           = 'nova-volumes',
+  $keystone_password      = 'keystone',
+  $keystone_enabled       = true,
+  $keystone_tenant        = 'services',
+  $keystone_user          = 'cinder',
+  $keystone_auth_host     = 'localhost',
+  $keystone_auth_port     = '35357',
+  $keystone_auth_protocol = 'http',
+  $service_port           = '5000',
+  $package_ensure         = 'latest',
+  $bind_host              = '0.0.0.0',
+  $config_api             = false,
+  $enabled                = true
 ) {
 
   class { 'cinder::base':
@@ -22,6 +33,22 @@ class openstack::cinder(
     class { 'cinder::volume::iscsi':
       volume_group     => $volume_group,
       iscsi_ip_address => $iscsi_ip_address,
+    }
+  }
+    
+  # Intentionally left the default as 'false' for backwards compatibility
+  if $config_api {
+    class { 'cinder::api':
+      keystone_password      => $keystone_password,
+      keystone_enabled       => $keystone_enabled,
+      keystone_tenant        => $keystone_tenant,
+      keystone_user          => $keystone_user,
+      keystone_auth_host     => $keystone_auth_host,
+      keystone_auth_port     => $keystone_auth_port,
+      keystone_auth_protocol => $keystone_auth_protocol,
+      #service_port           => $service_port,
+      package_ensure         => $package_ensure,
+      bind_host              => $bind_host,
     }
   }
 }
